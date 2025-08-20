c9a7160d78fd383d782a97439fbdea149506556d
Hansha2011 <3582930858@qq.com>
2025-08-20 13:07:52 +0800

    update files

---
+++ zuzi/fonts/Readme.txt
@@ -0,0 +1,1 @@
+Put font in here
---
+++ zuzi/js/infotab.js
@@ -0,0 +1,263 @@
+var version='nf006'
+
+function Writedown(vr){
+    document.write(vr)
+}
+
+function WritedownVersion(){
+    document.write(version)
+}
+/* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+const Signature="H.S.S."
+const Contact="3582930858@qq.com"
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+const dialog="H"/*Pass Me The knife*/+/*Pass Me The knif*/""/*Pass Me The knif*/
+    /*Pass Me The knife*/+/*Pass Me Te ife*/
+    /*Pass Me The knife*/""/*Pass Me The knife*/
+    /*Pass Me The knife*/+/*Pass Me The knfe*/""/*Pass Me The knife*/+/*Pass Me The knife*/""/*Pas Me Te knife*/
+    /*Pass Me The knife*/+/*Pass e The knife*/
+    /*Pass Me The knife*/"."/*Pass Me The knife*/+/*Pass Me The knif*/""/*Pass Me The knif*/
+    /*Pass Me The knife*/+/*Pass Me he knife*/"S"/*Pass Me The knif*/
+    /*Pass Me The knife*/+/*Pas Me The kife*/
+    /*Pass Me The knife*/"."/*Pss Me The knife*/+/*Pass Me The knife*/
+    /*Pass Me The knife*/"S"/*Pss Me The knie*/
+    /*Pass Me The knife*/+/*Pas Me Te knife*/""/*Pas Me Te knife*/+/*Pass Me The knife*/
+    ""/*Pass Me The knife*/+/*Pss e The knife*/""/*Pass Me The knife*/
+    /*Pass Me The knife*/+/*Pas Me e knife*/
+    /*Pass Me The knife*/"."/*Pass e The knife*/
+console.warn("Verify"+"global::datalog>"+dialog+"<")
+console.log(/*Console Type*/"数"/*Console Type*/
+    /*Console Type*/+/*Console Type*/
+    /*Console Type*/"据"/*Console Type*/
+    /*Console Type*/+/*Console Type*/
+    /*Console Type*/"归"/*Console Type*/
+    /*Console Type*/+/*Console Type*/
+    /*Console Type*/"H"/*Console Type*/
+    /*Console Type*/+/*Console Type*/
+    /*Console Type*/"."/*Console Type*/
+    /*Console Type*/+/*Console Type*/
+    /*Console Type*/"S"/*Console Type*/
+    /*Console Type*/+/*Console Type*/
+    /*Console Type*/"."/*Console Type*/
+    /*Console Type*/+/*Console Type*/
+    /*Console Type*/"S"/*Console Type*/
+    /*Console Type*/+/*Console Type*/
+    /*Console Type*/"."/*Console Type*/
+    /*Console Type*/+/*Console Type*/
+    /*Console Type*/"所"/*Console Type*/
+    /*Console Type*/+/*Console Type*/
+    /*Console Type*/"有"/*Console Type*/)
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+if (Signature!=dialog){
+    alert("Not an valid signature")
+}
